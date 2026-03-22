f99aed5fb60d2e4b64bb92a2ab139cc6e6907f84
yuesheng <yueshengliu30@gmail.com>
2026-03-21 22:13:05 -0500

    project setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from langchain-course!")
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 if __name__ == "__main__":
-    main()
+    print("Hello from langchain-course!")
